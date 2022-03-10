c9551dd8a1a2cd161bdbec8170f13c8676a6e98b
chamomiletay <chamomile.tay@gmail.com>
2022-03-09 22:50:15 -0500

    Changed on-screen keyboard from div squares to input buttons, added placeholder art, added on-screen score log

---
+++ script.js
@@ -0,0 +1,13 @@
+//Game Board
+    //Interactive image displays in middle of screen --> updates based on player input
+
+
+//Player
+    //Can interact with keyboard (on-screen or external)
+
+
+//Win Condition
+
+
+
+//Stretch Goals
