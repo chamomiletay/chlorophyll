63509a2d68358f5d79339a1ab03c59f868762e77
chamomiletay <chamomile.tay@gmail.com>
2022-03-09 23:15:49 -0500

    Whiteboarded ideas in JS, adjusted font size in p tags

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 //Game Board
-    //Interactive image displays in middle of screen --> updates based on player input
+    // interactive image displays in middle of screen --> updates based on player input
+    // on-screen keyboard accepts user input
+    // score and number of turns display on-screen
+    //correct letter responses will populate on screen into hidden message container
+    // incorrect letters should become disabled on keyboard once they are clicked once
 
 
 //Player
-    //Can interact with keyboard (on-screen or external)
-    //Incorrect letters should become disabled on keyboard once they are clicked once
+    // can interact with keyboard (on-screen or external)
+    // cannot click a letter more than once
+    // 
 
 
 //Win Condition
-
-
+    // player solves hidden message before number of turns is depleted
+    // "restart puzzle" button appears once message is decoded
 
 //Stretch Goals
+    // power-up: player can gain a "life" that prevents them from taking "damage" upon incorrect responses
+    // attach "how to play" link that links to set of instructions that player can access (or window prompt)
